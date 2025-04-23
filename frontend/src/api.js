38c08a7d718114215e43c58624ef34d0eaf83a0d
Talha <talha0pse@gmail.com>
2025-04-23 12:01:55 +0530

    backend render port update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // change to your Render URL after deployment
+    baseURL: 'https://copy-trading-backend-ksfs.onrender.com',
 });
 
 export default api;
