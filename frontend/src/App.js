493db0879d33aec990e312a7f0f45d10de424026
Talha <talha0pse@gmail.com>
2025-04-23 21:43:10 +0530

    Create src/TradeApp.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import PrivateRoute from './components/PrivateRoute';
-import TradeApp from './TradeApp'; // your existing trade UI
+import TradeApp from './TradeApp';      // <-- this now points to the file you created
 
 export default function App() {
   const logout = () => {
@@ -14,7 +15,10 @@ export default function App() {
   return (
     <BrowserRouter>
       <nav>
-        <Link to="/">Home</Link> | <Link to="/register">Register</Link> | <Link to="/login">Login</Link> | <button onClick={logout}>Logout</button>
+        <Link to="/">Home</Link> |{' '}
+        <Link to="/register">Register</Link> |{' '}
+        <Link to="/login">Login</Link> |{' '}
+        <button onClick={logout}>Logout</button>
       </nav>
       <Routes>
         <Route path="/login" element={<Login />} />
